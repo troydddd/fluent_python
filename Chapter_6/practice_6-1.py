3406dd8ac8e22a86eb446abdc335fbcaf8c92fe9
戴逸俊 <daiyijun@bytedance.com>
2019-09-10 21:24:19 +0800

    second

--- Chapter_6/practice_6-1.py
+++ Chapter_6/practice_6-1.py
@@ -4,3 +4,54 @@
 from abc import ABC, abstractmethod
 from collections import namedtuple
 
+Customer = namedtuple('Customer', 'name fidelity')
+
+
+class LineItem:
+
+    def __init__(self, product, quanity, price):
+        self.product = product
+        self.quantity = quanity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+
+class Order: # 上下文
+
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = list(cart)
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order Total: {:.2f} due {:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+class Promotion(ABC):
+
+    @abstractmethod
+    def discount(self, order):
+        """返回折扣金额"""
+
+
+class FidelityPromo(Promotion):
+    """积分为1000或以上的顾客提供5%折扣"""
+
+    def discount(self, order):
+        return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
+
