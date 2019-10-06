8e7b6be0e5ca906f226a09bc53cacdaf1a623867
戴逸俊 <daiyijun@bytedance.com>
2019-10-06 12:55:00 +0800

    chapter 10 vector update

---
+++ Chapter_10/vector_v1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from array import array
+import reprlib
+import math
+
+
+class Vector:
+    typecode = 'd'
+
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return 'Vector({})'.format(components)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(self._components))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
